8201fb09eac40e597d878a812199338acd3b63bd
goblegue <hmehboob.bese21seecs@seecs.edu.pk>
2024-08-05 19:02:53 +0500

    app ui created

--- ai-nft-generator/src/App.js
+++ ai-nft-generator/src/App.js
@@ -1,37 +1,102 @@
-import { useState, useEffect } from 'react';
-import { NFTStorage, File } from 'nft.storage'
-import { Buffer } from 'buffer';
-import { ethers } from 'ethers';
-import axios from 'axios';
+import { useState, useEffect } from "react";
+import { NFTStorage, File } from "nft.storage";
+import { Buffer } from "buffer";
+import { ethers } from "ethers";
+import axios from "axios";
 
 // Components
-import Spinner from 'react-bootstrap/Spinner';
-import Navigation from './components/Navigation';
+import Spinner from "react-bootstrap/Spinner";
+import Navigation from "./components/Navigation";
 
 // ABIs
-import NFT from './abis/NFT.json'
+import NFT from "./abis/NFT.json";
 
 // Config
-import config from './config.json';
+import config from "./config.json";
 
 function App() {
-  const [provider, setProvider] = useState(null)
-  const [account, setAccount] = useState(null)
+  const [provider, setProvider] = useState(null);
+  const [account, setAccount] = useState(null);
+  const [name, setName] = useState("");
+  const [description, setDescription] = useState("");
+  const [image, setImage] = useState(null);
 
   const loadBlockchainData = async () => {
-    const provider = new ethers.providers.Web3Provider(window.ethereum)
-    setProvider(provider)
-  }
+    const provider = new ethers.providers.Web3Provider(window.ethereum);
+    setProvider(provider);
+  };
+
+  const submitHandler = async (e) => {
+    e.preventDefault();
+
+    // const imageData = createImage();
+  };
+
+  // const createImage = async () => {
+  //   console.log("Generating Image...");
+
+  //   // You can replace this with different model API's
+  //   const URL = `https://api-inference.huggingface.co/models/stabilityai/stable-diffusion-3-medium-diffusers`;
+
+    // Send the request
+  //   const response = await axios({
+  //     url: URL,
+  //     method: "POST",
+  //     headers: {
+  //       Authorization: `Bearer ${process.env.REACT_APP_HUGGING_FACE_API_KEY}`,
+  //       Accept: "application/json",
+  //       "Content-Type": "application/json",
+  //     },
+  //     data: JSON.stringify({
+  //       inputs: description,
+  //       options: { wait_for_model: true },
+  //     }),
+  //     responseType: "arraybuffer",
+  //   });
+  //   const type = response.headers["content-type"];
+  //   const data = response.data;
+
+  //   const base64data = Buffer.from(data).toString("base64");
+  //   const img = 'data:${type};base64,' + base64data;
+  //   setImage(img);
+  //   return data; 
+  // };
 
   useEffect(() => {
-    loadBlockchainData()
-  }, [])
+    loadBlockchainData();
+  }, []);
 
   return (
     <div>
       <Navigation account={account} setAccount={setAccount} />
-      <p>Edit App.js to get started.</p>
-
+      <div className="form">
+        <form onSubmit={submitHandler}>
+          <input
+            type="text"
+            placeholder="Create a name..."
+            onChange={(e) => {
+              setName(e.target.value);
+            }}
+          ></input>
+          <input
+            type="text"
+            placeholder="Create a description..."
+            onChange={(e) => {
+              setDescription(e.target.value);
+            }}
+          ></input>
+          <input type="submit" placeholder="Create & Mint"></input>
+        </form>
+        <div className="image">
+          <img src={image} alt="Ai Generated Image" />
+        </div>
+      </div>
+      <p>
+        View&nbsp;
+        <a href="" target="_blank" rel="noreferrer">
+          Metadata
+        </a>
+      </p>
     </div>
   );
 }
